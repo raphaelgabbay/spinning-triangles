74ca5aa7538f33913ee2296f911a7f35d9df9558
raphaelgabbay <raphael.gabbay@gmail.com>
2019-04-15 13:32:52 +0800

    Changed fill opacity

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let spinCount = 0;
 let constantSpeed = 0;
 let moreRotation = 0;
 let halfTurn = false;
-// let arrowMode = true;
+let lockedMovement = false;
 let anglePoint = 0;
 
 //TODO Add dat.gui interface
@@ -18,6 +18,7 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
     noFill();
     stroke(255);
+    fill(0,100);
     angleMode(DEGREES);
 
     //Initialize trianglesArray
@@ -30,48 +31,71 @@ function draw() {
     if(!paused) {
         background(0);
 
-        trianglesArray.forEach(MyTriangle => {
-            MyTriangle.display();
-        });
-
-
-        //Update triangles
-        for(let i = 1; i < nbTriangles; i++) {            
-            trianglesArray[i].angle = lerp(trianglesArray[i].angle, trianglesArray[i-1].angle, 0.25);
-            trianglesArray[i].x = lerp(trianglesArray[i].x, trianglesArray[i-1].x, 0.15);
-            trianglesArray[i].y = lerp(trianglesArray[i].y, trianglesArray[i-1].y, 0.15);            
+        // trianglesArray.forEach(MyTriangle => {
+        //     MyTriangle.display();
+        // });
+        
+        for (let i = trianglesArray.length-1; i >= 0; i--) {
+            trianglesArray[i].display();
+            
         }
+        
+  
+        if(lockedMovement) {
 
-        if(mouseX < windowWidth/2) {
-            if (prevMouseY < windowHeight/2 && mouseY > windowHeight/2) {
-                spinCount--;
-            } else if (prevMouseY > windowHeight/2 && mouseY < windowHeight/2) {
-                spinCount++;
+            //Update first triangle angle
+            trianglesArray[0].angle = lerp(trianglesArray[0].angle, anglePoint, 0.25);
+            //Update other triangles angle  
+            for(let i = 1; i < nbTriangles; i++) {            
+                trianglesArray[i].angle = lerp(trianglesArray[i].angle, trianglesArray[i-1].angle, 0.25);        
             }
+            
+            
+            // Check if mouse makes more than a full turn around center of 1st triangle
+            if(mouseX < trianglesArray[0].x) {
+                if (prevMouseY < trianglesArray[0].y && mouseY > trianglesArray[0].y) {
+                    spinCount--;
+                } else if (prevMouseY > trianglesArray[0].y && mouseY < trianglesArray[0].y) {
+                    spinCount++;
+                }
+            }
+            anglePoint = 360*spinCount + atan2(mouseY - trianglesArray[0].y, mouseX - trianglesArray[0].x)+90;
+
+        } else { //If !lockedMovement
+
+            //Update first triangle angle && position
+            trianglesArray[0].angle = lerp(trianglesArray[0].angle, anglePoint, 0.25);
+            trianglesArray[0].x = lerp(trianglesArray[0].x, mouseX, 0.25);
+            trianglesArray[0].y = lerp(trianglesArray[0].y, mouseY, 0.25);
+
+            //Update other triangles angle && position
+            for(let i = 1; i < nbTriangles; i++) {            
+                trianglesArray[i].angle = lerp(trianglesArray[i].angle, trianglesArray[i-1].angle, 0.25);
+                trianglesArray[i].x = lerp(trianglesArray[i].x, trianglesArray[i-1].x, 0.15);
+                trianglesArray[i].y = lerp(trianglesArray[i].y, trianglesArray[i-1].y, 0.15);            
+            }
+
+            // Check if mouse makes more than a full turn around center of window
+            if(mouseX < windowWidth/2) {
+                if (prevMouseY < windowHeight/2 && mouseY > windowHeight/2) {
+                    spinCount--;
+                } else if (prevMouseY > windowHeight/2 && mouseY < windowHeight/2) {
+                    spinCount++;
+                }
+            }
+            anglePoint = 360*spinCount + atan2(mouseY - height / 2, mouseX - width / 2)+90;
+            
         }
+        
 
         prevMouseY = mouseY;
-        
-        // if(!arrowMode) {
-            anglePoint = 360*spinCount + atan2(mouseY - height / 2, mouseX - width / 2)+90;
-        // } else {
-            moreRotation += constantSpeed;
-            anglePoint += moreRotation;
-        // }
+        moreRotation += constantSpeed;
+        anglePoint += moreRotation;
         
         if(halfTurn) {
             halfTurn = false;
-            if(spinCount == 0) {
-                spinCount = 1;
-            } else {
-                spinCount = 0;
-            }
+            spinCount = random(-10, 10);
         }
-        
-        //Update first triangle
-        trianglesArray[0].angle = lerp(trianglesArray[0].angle, anglePoint, 0.25);
-        trianglesArray[0].x = lerp(trianglesArray[0].x, mouseX, 0.25);
-        trianglesArray[0].y = lerp(trianglesArray[0].y, mouseY, 0.25);
     }
 }
 
@@ -117,3 +141,6 @@ function keyPressed() {
 //     arrowMode = false;
 //     constantSpeed = 0;
 // }
+function mouseClicked() {
+    lockedMovement = !lockedMovement;
+}
